e52eeacc06d939f860896bca5376ddacef115c12
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2023-04-14 00:05:45 -0300

    inicio exemplo api

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace primeira_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet("ObterDataHoraAtual")]
+        public IActionResult ObterDatahora() {
+
+            var obj = new {
+                Data = DateTime.Now.ToLongDateString(),
+                Hora = DateTime.Now.ToShortTimeString()
+            };
+
+            return Ok(obj);
+        }
+    }
+}
